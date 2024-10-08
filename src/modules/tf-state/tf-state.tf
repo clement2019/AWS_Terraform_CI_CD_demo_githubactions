b97e673407d1df26d66f23ae0e6a42ede8d177ed
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-10-08 16:51:28 +0100

    update

--- src/modules/tf-state/tf-state.tf
+++ src/modules/tf-state/tf-state.tf
@@ -19,15 +19,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
     }
   }
 }
-
-# Dynamo DB Table for Locking TF Config
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "-lock=false"
-  
-  attribute {
-    name = "-lock"
-    type = "S"
-  }
-}
