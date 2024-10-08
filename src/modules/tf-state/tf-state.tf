20f5d1fd7d824d515924957e16051e5b8cc08cd2
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-10-08 16:37:42 +0100

    initail commit4

--- src/modules/tf-state/tf-state.tf
+++ src/modules/tf-state/tf-state.tf
@@ -24,7 +24,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  hash_key     = "-lock=false"
+  
   attribute {
     name = "LockID"
     type = "S"
